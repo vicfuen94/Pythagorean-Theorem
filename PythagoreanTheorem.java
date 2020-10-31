65d9539ca62acb5506b7c36acd69a238954b4627
vicfuen94 <fuentesvictoria923@gmail.com>
2020-10-30 19:17:40 -0500

    completed Pythagorean Thoerem

---
+++ PythagoreanTheorem.java
@@ -0,0 +1,6 @@
+// import java.util.Math;
+public class PythagoreanTheorem {
+    public static double calculateHypotenuse (int lenA, int lenB) {
+        return Math.sqrt((lenA * lenA) + (lenB * lenB));
+    }
+}
---
+++ PythagoreanTheoremTest.java
@@ -0,0 +1,8 @@
+public class PythagoreanTheoremTest {
+    public static void main(String[] args){
+        // double four = 4.0;
+        // double squareroot = Math.sqrt(four);
+        PythagoreanTheorem pt = new PythagoreanTheorem();
+        System.out.println(pt.calculateHypotenuse(6,36));
+    }
+}
